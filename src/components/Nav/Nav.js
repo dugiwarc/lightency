5b88600c17caff451e6c8b3c5ec94cd406e30447
Dugiwarc <gicubotnaru@gmail.com>
2019-08-14 13:40:35 +0200

    background filler fix

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -39,7 +39,7 @@ export default class Nav extends Component {
       position: "absolute",
       background: "black",
       filter: "opacity(0.6)",
-      width: "100vw",
+      width: "100%",
       height: "400vh",
       zIndex: "200",
       display: this.state.hamburgerIconIsClicked ? "block" : "none"
