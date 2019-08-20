b6e5a3572b316d70abd462e8846ea785d6ed86c3
Dugiwarc <gicubotnaru@gmail.com>
2019-08-20 12:48:37 +0200

    fix animations @HomeText

--- src/components/HomeText/HomeText.js
+++ src/components/HomeText/HomeText.js
@@ -6,49 +6,62 @@ import HomeButton from "../HomeButton/HomeButton";
 // Styles
 import "./HomeText.scss";
 
-const HomeText = () => {
-  return (
-    <div className="Home-text">
-      <div className="Home-text-title">
-        <p>
-          <span className="text-gold">
-            <span className="text-effect" />
-            DEMOCRATIZE
-          </span>
-          {"\u00A0"}
-          ACCESS TO GREEN{"\u00A0"}
-          <span className="text-gold">
-            <span className="text-effect second" />
-            ENERGY
-          </span>
-          .
-        </p>
-      </div>
-      <div className="Home-text-subtitle">
-        <p>
-          <span className="text-gold">
-            <span className="text-effect third" />
-            Lightency
-          </span>{" "}
-          is a green tech startup that harnesses the power of deep technologies
-          to ensure/accelerate better access to affordable and green energy.
-        </p>
-        <p>
-          By providing a decentralized solution, we ensure that green energy is
-          produced, consumed, and exchanged locally which lowers the cost and
-          increases efficiency.
-        </p>
-      </div>
-      <div className="buttons">
-        <div className="Home-text-button-left">
-          <HomeButton text="Watch video" />
+class HomeText extends React.Component {
+  state = { isReady: false };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isReady: true });
+    }, 300);
+  }
+  render() {
+    const subtitleStyles = {
+      transform: this.state.isReady && "translateY(0px)",
+      opacity: this.state.isReady && "1"
+    };
+    return (
+      <div className="Home-text">
+        <div className="Home-text-title">
+          <p>
+            <span className="text-gold">
+              <span className="text-effect" />
+              DEMOCRATIZE
+            </span>
+            {"\u00A0"}
+            ACCESS TO GREEN{"\u00A0"}
+            <span className="text-gold">
+              <span className="text-effect second" />
+              ENERGY
+            </span>
+            .
+          </p>
         </div>
-        <div className="Home-text-button-right">
-          <HomeButton text="Join us" joinUs />
+        <div className="Home-text-subtitle" style={subtitleStyles}>
+          <p>
+            <span className="text-gold">
+              <span className="text-effect third" />
+              Lightency
+            </span>{" "}
+            is a green tech startup that harnesses the power of deep
+            technologies to ensure/accelerate better access to affordable and
+            green energy.
+          </p>
+          <p>
+            By providing a decentralized solution, we ensure that green energy
+            is produced, consumed, and exchanged locally which lowers the cost
+            and increases efficiency.
+          </p>
+        </div>
+        <div className="buttons" style={subtitleStyles}>
+          <div className="Home-text-button-left">
+            <HomeButton text="Watch video" />
+          </div>
+          <div className="Home-text-button-right">
+            <HomeButton text="Join us" joinUs />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default HomeText;
