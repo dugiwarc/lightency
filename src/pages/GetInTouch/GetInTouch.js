f381fbe33e8a1196f33f9ece306818483670ba0e
dugiwarc <gicubotnaru@gmail.com>
2019-08-22 16:08:55 -0700

    add animations @GetInTouch

--- src/pages/GetInTouch/GetInTouch.js
+++ src/pages/GetInTouch/GetInTouch.js
@@ -3,31 +3,55 @@ import React from "react";
 import "./GetInTouch.scss";
 
 class GetInTouch extends React.Component {
-  componentDidMount() {
-    window.scrollTo(0, 0);
-  }
-  render() {
-    return (
-      <div className="GetInTouch">
-        <form>
-          <h1>Let's talk</h1>
-          <div>
-            <label htmlFor="email">Email*</label>
-            <input id="email" type="text" required />
-          </div>
-          <div>
-            <label htmlFor="name">Name*</label>
-            <input id="name" type="text" required />
-          </div>
-          <div>
-            <label htmlFor="message">What can we help you with?*</label>
-            <textarea id="message" name="message" />
-          </div>
-          <button type="submit">Send</button>
-        </form>
-      </div>
-    );
-  }
+	state = {
+		title: "Let's talk",
+		isReady: false
+	};
+	componentDidMount() {
+		window.scrollTo(0, 0);
+		setTimeout(() => {
+			this.setState({ isReady: true });
+		}, 100);
+	}
+	render() {
+		const letterStyles = {
+			transform: this.state.isReady && "translateY(0px)"
+		};
+		return (
+			<div className='GetInTouch'>
+				<form>
+					<div className='GITTitle'>
+						{this.state.title.split("").map((item, index) => {
+							return (
+								<div key={index} className='letter' style={letterStyles}>
+									{`${item} ${index === 4 ? "\u00A0" : ""}`}
+								</div>
+							);
+						})}
+					</div>
+					<div>
+						<label htmlFor='email' style={letterStyles}>
+							Email*
+						</label>
+						<input id='email' type='text' required />
+					</div>
+					<div>
+						<label htmlFor='name' style={letterStyles}>
+							Name*
+						</label>
+						<input id='name' type='text' required />
+					</div>
+					<div>
+						<label htmlFor='message' style={letterStyles}>
+							What can we help you with?*
+						</label>
+						<textarea id='message' name='message' />
+					</div>
+					<button type='submit'>Send</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default GetInTouch;
