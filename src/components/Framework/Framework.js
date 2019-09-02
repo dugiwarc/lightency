7447f8f42a5b4548022d043abdde062e550c3178
dugiwarc <gicubotnaru@gmail.com>
2019-09-02 12:18:13 +0200

    refactor framework IS

--- src/components/Framework/Framework.js
+++ src/components/Framework/Framework.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Lottie from "lottie-react-web";
 
 import "./Framework.scss";
+
+import data from "../../data/framework.data";
 
 const Framework = () => {
 	const defaultOptions = {
@@ -15,56 +17,17 @@ const Framework = () => {
 			<div className='lottie'>
 				<div className='lottie-obj'>
 					<div className='patches'>
-						<div className='one'>
-							{/* <div className="title">Smart Water Management</div> */}
-							{/* <div className="desc">
-                Collect, share and analyze data from water equipment and water
-                networks to find leaks, lower energy use and predict equipement
-                failure
-              </div> */}
-						</div>
-						<div className='child-1'>
-							Collect, share and analyze data from water equipment and water
-							networks to find leaks, lower energy use and predict equipement
-							failure
-						</div>
-						<div className='two'></div>
-						<div className='child-2'>
-							Farming management using modern Information and Communication
-							Technologies to increase the quantity and quality of products...
-						</div>
-						<div className='three'></div>
-						<div className='child-3'>
-							Innovative building technology for the developement and
-							construction ...
-						</div>
-						<div className='four'></div>
-						<div className='child-4'>
-							More efficient, reliable and transparent processes .
-						</div>
-						<div className='five'></div>
-						<div className='child-5'>
-							Create less congestion, better infrastructure, better access to
-							public transport and cleaner air...
-						</div>{" "}
-						<div className='seven'></div>
-						<div className='child-7'>
-							Tokenization of assets using the blockchain solutions
-						</div>
-						<div className='eight'></div>
-						<div className='child-8'>
-							Using e-governance to strengthen democracy, citizen participation
-							and public welfare.
-						</div>
-						<div className='nine'></div>
-						<div className='child-9'>
-							Applying data mining techniques to provide proper data for
-							building’s optimal operation, enabling P2P trading of renewabe
-							energy...
-						</div>
+						{data.map(item => {
+							return (
+								<Fragment>
+									<div className={`${item.className}`}></div>
+									<div className={`child-${item.id}`}>{item.description}</div>
+								</Fragment>
+							);
+						})}
 					</div>
 					<div className='object'>
-						<Lottie options={defaultOptions} overflow={"none"} speed={1} />
+						<Lottie options={defaultOptions} overflow={"none"} />
 					</div>
 				</div>
 			</div>
